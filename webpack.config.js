e73434155d5604c3682b0a92249f00db0f60e87e
DESKTOP-P0ALHGR\Youn <yjh6406@gmail.com>
2018-12-24 16:45:23 +0900

    공부사항_크리스마스이브_일괄커밋

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './app/cssTest.js',
@@ -7,9 +8,23 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
+     //.css 파일이 아닌 js로 넣는 방법
+    // rules: [{
+    //   test: /\.css$/,
+    //   use: ['style-loader', 'css-loader']
+    // }]
+    // .css 파일로 만드는 방법
     rules: [{
       test: /\.css$/,
-      use: ['style-loader', 'css-loader']
+      // Comment this out to load ExtractTextPlugin
+      // use: ['style-loader', 'css-loader']
+      use: ExtractTextPlugin.extract({
+        fallback: "style-loader",
+        use: "css-loader"
+      })
     }]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css')
+  ]
 }
