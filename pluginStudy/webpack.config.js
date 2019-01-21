7312123baf79484a8ebe3790e9966b575c0b0c87
DESKTOP-P0ALHGR\Youn <yjh6406@gmail.com>
2019-01-21 17:37:49 +0900

    no message

--- pluginStudy/webpack.config.js
+++ pluginStudy/webpack.config.js
@@ -1,17 +1,46 @@
 var webpack = require('webpack');
 var path = require('path');
+var ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
-  entry: {
-    main: './app/index.js',
-    vendor: [
-      'moment',
-      'lodash'
-    ]
-  },
-  output: {
-    // entry 에서 설정한 이름으로 저장이 된다.
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
-  }
-}
+        entry: {
+                main: './app/index.js',
+                vendor: [
+                        'moment',
+                        'lodash'
+                ]
+        },
+        output: {
+                // entry 에서 설정한 이름으로 저장이 된다.
+                filename: '[name].js',
+                path: path.resolve(__dirname, 'dist')
+        },
+        // plugins: [
+        //         new webpack.optimize.SplitChunksPlugin({
+        //                 optimization: {
+        //                         splitChunks: {
+        //                                 cacheGroups: {
+        //                                         vendor: {
+        //                                                 chunks: 'initial',
+        //                                                 name: 'vendor',
+        //                                                 enforce: true,
+        //                                         },
+        //                                 },
+        //                         },
+        //                         runtimeChunk: {
+        //                                 name: 'manifest',
+        //                         }
+        //                 },
+        //         })
+        // ]
+        plugins: [
+                new webpack.ProvidePlugin({
+                        names: ["vendor", "manifest"]
+                }),
+                new ManifestPlugin({
+                        fileName: "manifest.json",
+                        basePath: ". / dist / "
+                })
+        ]
+
+};
